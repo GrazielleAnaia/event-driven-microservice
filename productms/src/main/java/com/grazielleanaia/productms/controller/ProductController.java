36bd053e1264b44ece1085f4a447ad40b85f1333
Grazielle <grazielle.anaia@gmail.com>
2026-01-14 12:11:25 -0500

    Added acknowledgements and retries to application.yml and KafkaConfig class

--- productms/src/main/java/com/grazielleanaia/productms/controller/ProductController.java
+++ productms/src/main/java/com/grazielleanaia/productms/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.grazielleanaia.productms.controller;
 
 
 import com.grazielleanaia.productms.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @RequestMapping("/products")
 
 public class ProductController {
-
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
     ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -21,11 +25,18 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<String> createProduct(@RequestBody CreateProductRequest product) {
-        String productId = productService.createProduct(product);
+    public ResponseEntity<Object> createProduct(@RequestBody CreateProductRequest product) {
 
+        String productId = null;
+        try {
+            productId = productService.createProduct(product);
+        } catch (Exception e) {
+//            e.printStackTrace();
+            LOGGER.error(e.getMessage(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage(new Date(),
+                    e.getMessage(), "/products"));
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body("Product successfully created. " + productId);
     }
-
 
 }
